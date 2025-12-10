7386bd4d0f90fee283b8c405f7fc61d7bb70d69c
ARYAN ROHELA <97533934+A-r-y-a-n1@users.noreply.github.com>
2025-12-10 06:06:52 +0530

    amazon oa question with pbds
    count of subarrays with average greater than or equal to k, using pbds

--- file.cpp
+++ file.cpp
@@ -1,5 +1,9 @@
 #include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+#define ordered_set tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
 
 int main(){
     vector<int> a = {1,2,3};
@@ -9,14 +13,18 @@ int main(){
         p[i] += a[i-1] + p[i-1];
     }
 
-    multiset<int> s;
+    ordered_set s;
+    unordered_map<int, int> mp;
     int count = 0;
     for(int i = 1 ; i <= n ; i++){
-        s.insert(p[i-1] - k*i);
+        int lhs = p[i-1] - k*i;
+        mp[lhs]++;
+        int freq = mp[lhs]; 
+        s.insert({lhs,freq});
         int d = p[i] - k*i - k;
         cout<< "d -> "<< d <<" ";
         // how many are there lesser or equal to d
-        int e = distance(s.begin(), s.upper_bound(d));
+        int e = s.order_of_key({d+1,0});
         count += e;
         cout <<"e -> " << e << endl << endl;
     }
